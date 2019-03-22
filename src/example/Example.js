fb6ce43ff9a78ebf69c462d97b9fe2d76caa53df
Grenysherg <grenysherg@gmail.com>
2019-03-22 09:46:19 +0300

    add example component

---
+++ src/example/Example.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import './Example.css';
+
+export class Example extends Component {
+  render() {
+    return <button className="Example" type="button">Text</button>;
+  }
+}
